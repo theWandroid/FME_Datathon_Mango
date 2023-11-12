3a3ee183570baa81fd42fcbdd2bbb9f973181e7c
theWandroid <68104028+theWandroid@users.noreply.github.com>
2023-11-12 11:13:20 +0100

    one more update

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 import file from "./product_data.json" assert {type: 'json' }
 
-console.log(file);
+const app = document.getElementById('root');
+let num = Math.floor(Math.random() * 5);
+for (let i = 0; i < num; i++) {
+    const imagen = document.createElement('img');
+    let random_num = Math.floor(Math.random() * file.length);
+const source = file[random_num]["des_filename"];
+app.appendChild(imagen);
+imagen.setAttribute("src", source);
+  }
+console.log(file.length);
